34864f6ec6e6c8cee821db73d5aa872a4d08000b
pauloarn <paulo.amador98@gmail.com>
2024-03-04 21:30:31 -0300

    Ajustando script de seed

---
+++ src/main/resources/scripts/migrations/V2.0__initial_seed.sql
@@ -0,0 +1,100 @@
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (1, 'Plyometrics', 'Pliometria', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (2, 'Stretching', 'Alongamento', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (3, 'Thighs', 'Coxas', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (4, 'Forearms', 'Antebraços', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (5, 'Back', 'Costa', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (6, 'Waist', 'Cintura', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (7, 'Chest', 'Peito', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (8, 'Hips', 'Quadris', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (9, 'Cardio', 'Cardio', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (10, 'Upper Arms', 'Braços superiores', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (11, 'Shoulders', 'Ombros', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (12, 'Neck', 'Pescoço', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (13, 'Calves', 'Panturrilha', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (14, 'Weightlifting', 'Levantamento de peso', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (15, 'Full body', 'Corpo todo', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (16, 'Adbomen', 'Abdomen', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (17, 'Pilates', 'Pilates', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.BodyPart (id, name, nome, createdAt, updatedAt)
+values (18, 'Yoga', 'Ioga', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (1, 'Body weight', 'Peso corporal', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (2, 'Band', 'Elástico', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (3, 'Wheight', 'Peso', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (4, 'Wheel roller', 'Rolo de roda', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (5, 'Medicine Ball', 'Medicine Ball', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (6, 'Dumbbell', 'Haltere', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (7, 'Leverage machine', ' Máquina de alavancagem', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (8, 'Stability ball', 'Bola de estabilidade', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (9, 'Cable', 'Cabo', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (10, 'Trap bar', 'Barra Hexagonal', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (11, 'Suspension', 'Suspensão', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (12, 'Barbell', 'Barra', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (13, 'Rope', 'Corda', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (14, 'Bosu ball', 'Meia Bola Suiça', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (15, 'Smith machine', 'Smith', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (16, 'Hammer', 'Martelo', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (17, 'Sled machine', 'Máquina de trenó', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (18, 'Esteira', 'Esteira', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (19, 'Rollball', 'Rollball', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (20, 'Roll', 'Rolinho', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (21, 'Resistance Band', 'Faixa de Resistência', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (22, 'Stick', 'Barra', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (23, 'Power Sled', 'Trenó de Peso', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (24, 'Olympic barbell', 'Barra olímpica', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (25, 'Hack', 'Hack', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (26, 'Kettlebell', 'Kettlebell', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (27, 'EZ Barbell', 'Barra W', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (28, 'Battling Rope', 'Corda Naval', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.equipment_type (id, name, nome, createdAt, updatedAt)
+values (29, 'Assisted', 'Assistido', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.exercise_type (id, name, nome, createdAt, updatedAt)
+values (1, 'Aerobic', 'Aeróbico', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.exercise_type (id, name, nome, createdAt, updatedAt)
+values (2, 'Stretching', 'Alongamento', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
+insert into FitApp.exercise_type (id, name, nome, createdAt, updatedAt)
+values (3, 'Strength', 'Força', '2023-05-15 20:08:00.000', '2023-05-15 20:08:00.000');
