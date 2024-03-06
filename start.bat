c980955637ce8052b84666418f0e6e1c4497bf81
pauloarn <paulo.amador98@gmail.com>
2024-03-06 10:34:14 -0300

    Iniciando Actions

--- start.bat
+++ start.bat
@@ -1,5 +1,10 @@
 @ECHO OFF
 setlocal
+set "DB_NAME=FitDataBase"
+set "DB_CONNECTION_USER_NAME=root"
+set "DB_CONNECTION_PASSWORD=fitAppStrong@123"
+set "DB_CONNECTION_HOST=192.168.0.26"
+
 set "JAVA_HOME=C:\Program Files\Amazon Corretto\jdk11.0.21_9"
 set "path=%JAVA_HOME%;%path%"
 mvn clean install -DskipTests spring-boot:run
