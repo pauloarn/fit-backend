e706b4599db58b7365194a93f1814c72b0fca90f
pauloarn <paulo.amador98@gmail.com>
2024-03-08 16:08:20 -0300

    Update SecurityConfig.java

--- src/main/java/com/bolo/fit/config/SecurityConfig.java
+++ src/main/java/com/bolo/fit/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,11 +27,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthFilter authFilter;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
-            .cors().disable()
+            .cors().and()
             .httpBasic().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests(auth ->
